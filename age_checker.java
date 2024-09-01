2df3d406e3be136ca4c76b61bfeecd939c0c1395
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-09-01 15:51:05 +0800

    added separators

--- age_checker.java
+++ age_checker.java
@@ -5,19 +5,28 @@ public class age_checker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        separator();
         System.out.print("Enter Age: ");
         int age = scanner.nextInt();
 
+        separator();
         if (age <= 1 ){
             System.out.println("You haven't been born yet");
+            separator();
         }
 
         else if (age < 18){
             System.out.print("You are a Minor");
+            separator();
         }
 
         else {
             System.out.println("You are an adult");
+            separator();
         }
+    }
+
+    private static void separator(){
+        System.out.print("--------------------\n");
     }
 }
